a3402fd5165a9fe01a4166ff092d3abc8fde5379
Alexey-Gorulev-kernelics <99732548+Alexey-Gorulev-kernelics@users.noreply.github.com>
2023-07-06 15:16:00 +0300

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,2 @@
-cd $env:APPVEYOR_ERANGE_BACKEND_PATH
-echo $env:APPVEYOR_BUILD_VERSION > reload.log
-cd $env:APPVEYOR_ERANGE_FRONTEND_PATH
-echo $env:APPVEYOR_BUILD_VERSION > reload.log
-
 cd $env:APPLICATION_PATH
 gci env:* | sort-object name > env.log
